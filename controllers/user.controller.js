d58e23173c75ca84a62eda0444913e95e3b69c59
Dolly Aggarwal <dollyaggarwal1712@gmail.com>
2024-01-24 16:04:08 +0530

    completed user functionality.

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -22,3 +22,48 @@ module.exports.createSession = function (req, res) {
 	return res.redirect('/');
 };
 
+module.exports.signout = function (req, res) {
+	req.logout(function (err) {
+		if (err) {
+			return next(err);
+		}
+	});
+	return res.redirect('/users/signin');
+};
+
+// create user
+module.exports.createUser = async function (req, res) {
+	const { name, email, password, confirmPassword } = req.body;
+	try {
+		if (password !== confirmPassword) {
+			console.log(`Passwords dont match`);
+			return res.redirect('back');
+		}
+		const user = await User.findOne({ email });
+
+		if (user) {
+			console.log(`Email already exists`);
+			return res.redirect('back');
+		}
+
+		const newUser = await User.create({
+			name,
+			email,
+			password,
+		});
+
+		await newUser.save();
+
+		if (!newUser) {
+			console.log(`Error in creating user`);
+			return res.redirect('back');
+		}
+
+		return res.redirect('/users/signin');
+	} catch (error) {
+		console.log(`Error in creating user: ${error}`);
+		res.redirect('back');
+	}
+};
+
+
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -6,6 +6,8 @@ const userRouter = express.Router();
 
 userRouter.get('/signup', userController.signup);
 userRouter.get('/signin', userController.signin);
+userRouter.post('/create', userController.createUser);
+userRouter.post('/create-session', passport.authenticate('local', { failureRedirect: '/users/signin' }), userController.createSession);
 
 module.exports = userRouter;
 
